9de4fdacb4c4d00d0c3c0ed7496330770a2c435f
farisabqori <farisabq99@gmail.com>
2025-05-22 15:49:29 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
 //import java.io
 
 //System.out.println("Hello World")
@@ -51,3 +55,15 @@ Route::get('frontend', function () {
 Route::get('fronen', function () {
 	return view('fronen');
 });
+
+Route::get('dosen',[DosenController::class,'index']);
+Route::get('welcome',[DosenController::class,'welcome']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class,'home']);
+Route::get('/blog/tentang', [BlogController::class,'tentang']);
+Route::get('/blog/kontak', [BlogController::class,'kontak']);
