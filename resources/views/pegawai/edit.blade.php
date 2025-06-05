b08f4d452333834451b6b3d6d3c3c07f822519c7
farisabqori <farisabq99@gmail.com>
2025-06-05 16:26:00 +0700

    Pertemuan 14

---
+++ resources/views/pegawai/edit.blade.php
@@ -0,0 +1,72 @@
+@extends('../template')
+
+@section('content')
+	<h3>Edit Pegawai</h3>
+
+	<a href="/pegawai" class="btn btn-info"> Kembali</a>
+
+	<br/>
+	<br/>
+
+	@foreach($pegawai as $p)
+	<form action="/pegawai/update" method="post">
+		{{ csrf_field() }}
+		<input type="hidden" name="id" value="{{ $p->pegawai_id }}">
+    <div class="form-group has-success">
+        <label class="control-label col-sm-2" for="id1">
+            Nama
+        </label>
+        <div class="col-sm-6">
+            <input class="form-control"
+                    type="text"
+                    id="id1"
+                    placeholder="Input nama pegawai"
+                    name="nama"
+                    required="required"
+                    value="{{ $p->pegawai_nama }}">
+        </div>
+    </div>
+    <div class="form-group has-success">
+        <label class="control-label col-sm-2" for="id2">
+            Jabatan
+        </label>
+        <div class="col-sm-6">
+            <input class="form-control"
+                    type="text"
+                    id="id2"
+                    placeholder="Input jabatan pegawai"
+                    name="jabatan"
+                    required="required"
+                    value="{{ $p->pegawai_jabatan }}">
+        </div>
+    </div>
+    <div class="form-group has-success">
+        <label class="control-label col-sm-2" for="id3">
+            Umur
+        </label>
+        <div class="col-sm-6">
+            <input class="form-control"
+                    type="text"
+                    id="id3"
+                    placeholder="Input umur pegawai"
+                    name="umur"
+                    required="required"
+                    value="{{ $p->pegawai_umur }}">
+        </div>
+    </div>
+    <div class="form-group has-success">
+        <label class="control-label col-sm-2" for="id4">
+            Alamat
+        </label>
+        <div class="col-sm-6">
+            <textarea class="form-control"
+                    id="id4"
+                    placeholder="Input alamat pegawai"
+                    name="alamat"
+                    required="required">{{ $p->pegawai_alamat }}</textarea>
+        </div>
+    </div>
+		<input type="submit" class="btn btn-primary" value="Simpan Data">
+	</form>
+	@endforeach
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
-use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\Pegawai2Controller;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
+
 
 //import java.io
 
@@ -59,11 +61,21 @@ Route::get('fronen', function () {
 Route::get('dosen',[DosenController::class,'index']);
 Route::get('welcome',[DosenController::class,'welcome']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
-Route::get('/formulir', [PegawaiController::class,'formulir']);
-Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+//Route::get('/pegawai/{nama}', [Pegawai2Controller::class,'index']);
+Route::get('/formulir', [Pegawai2Controller::class,'formulir']);
+Route::post('/formulir/proses', [Pegawai2Controller::class,'proses']);
 
 // route blog
 Route::get('/blog', [BlogController::class,'home']);
 Route::get('/blog/tentang', [BlogController::class,'tentang']);
 Route::get('/blog/kontak', [BlogController::class,'kontak']);
+
+//pegawaiDB
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']); //jika form dikirim, route ini akan dijalankan
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
+
